db51326c8c9f9190bb0ee9f437b21a0f6268c6ba
Semyon Diulin <semzzcs@gmai.com>
2024-10-02 23:02:46 +0300

    вм4

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
         WashingMachine wm1 = new WashingMachine(60, 50);
         WashingMachine wm2 = new WashingMachine(60, 20);
         WashingMachine wm3 = new WashingMachine(60, 33);
+        WashingMachine wm4 = new WashingMachine(633, 333);
 
         System.out.println(wm3.Load("светлое", 20));
         System.out.println(wm3.Load("светлое", 14));
